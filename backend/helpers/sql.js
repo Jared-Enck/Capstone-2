50f07253d345fdae0e7dc8f52d67a466a7fce875
Jared Enck <jaredenck86@gmail.com>
2023-04-27 15:47:52 -0400

    create sql helper for partial user update

---
+++ backend/helpers/sql.js
@@ -0,0 +1,22 @@
+const { BadRequestError } = require("../expressError");
+
+/** Converts js variables to sql column names and sets up data values to update 
+ * Returns {setCols (column names converted), values}
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {isAdmin: false} => ['"is_admin"=$1']
+  const cols = keys.map((colName, idx) =>
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
