16a77eb2e3aed8c182d5ebacaa23395711cf2613
Jared Enck <jaredenck86@gmail.com>
2023-04-29 18:44:32 -0400

    add tests for token.js

--- backend/helpers/tokens.test.js
+++ backend/helpers/tokens.test.js
@@ -3,34 +3,12 @@ const { createToken } = require("./tokens");
 const { SECRET_KEY } = require("../config");
 
 describe("createToken", function () {
-  it("works: not admin", function () {
-    const token = createToken({ username: "test", is_admin: false });
-    const payload = jwt.verify(token, SECRET_KEY);
-    expect(payload).toEqual({
-      iat: expect.any(Number),
-      username: "test",
-      isAdmin: false,
-    });
-  });
-
-  it("works: admin", function () {
-    const token = createToken({ username: "test", isAdmin: true });
-    const payload = jwt.verify(token, SECRET_KEY);
-    expect(payload).toEqual({
-      iat: expect.any(Number),
-      username: "test",
-      isAdmin: true,
-    });
-  });
-
-  it("works: default no admin", function () {
-    // given the security risk if this didn't work, checking this specifically
+  it("works", function () {
     const token = createToken({ username: "test" });
     const payload = jwt.verify(token, SECRET_KEY);
     expect(payload).toEqual({
       iat: expect.any(Number),
-      username: "test",
-      isAdmin: false,
+      username: "test"
     });
   });
 });
