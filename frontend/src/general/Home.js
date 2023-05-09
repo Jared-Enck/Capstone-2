e18e997e18b0b14bc57107d1ab25865e9e5f5c32
Jared Enck <jaredenck86@gmail.com>
2023-05-09 15:36:37 -0400

    create homepage comp

---
+++ frontend/src/general/Home.js
@@ -0,0 +1,20 @@
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+export default function Home() {
+  return (
+    <Container maxWidth='lg'>
+      <h2>
+        Homepage.
+      </h2>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="flex-start"
+      >
+        
+      </Grid>
+    </Container>
+  );
+};
