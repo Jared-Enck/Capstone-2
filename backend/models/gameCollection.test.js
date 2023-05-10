7be7711ee6dc9d063812a792351db247d0ba55b0
Jared Enck <jaredenck86@gmail.com>
2023-05-10 14:48:55 -0400

    added testing for GameCollections addGame and removeGame

---
+++ backend/models/gameCollection.test.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const db = require("../db.js");
+const User = require("./user");
+const { 
+  BadRequestError,
+  NotFoundError
+} = require("../expressError.js");
+const GameCollection = require("./gameCollection.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll
+} = require("../_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** addGame */
+
+describe("addGame", () => {
+  const addGameObj = {
+    userID: 1,
+    game: {
+      id: "78ZDzlpvdb",
+      name: "Moonrakers"
+    }
+  };
+  it("works", async () => {
+    const {user} = await User.get(1);
+
+    expect(user.games.has("78ZDzlpvdb")).toBe(false);
+
+    const res = await GameCollection.addGame(addGameObj);
+
+    const updatedUser = (await User.get(1)).user;
+
+    expect(updatedUser.games.has("78ZDzlpvdb")).toBe(true);
+    expect(res).toEqual({
+      msg: `Moonrakers has been added to your collection.`
+    });
+  });
+});
+
+/************************************** removeGame */
+
+describe("removeGame", () => {
+  const removeGameObj = {
+    userID: 1,
+    game: {
+      id: "nSZTnbgacm",
+      name: "Dinosaur Island"
+    }
+  }
+
+  it("works", async () => {
+    const {user} = await User.get(1);
+
+    expect(user.games.has("nSZTnbgacm")).toBe(true);
+
+    const res = await GameCollection.removeGame(removeGameObj);
+
+    const updatedUser = (await User.get(1)).user;
+
+    expect(updatedUser.games.has("nSZTnbgacm")).toBe(false);
+    expect(res).toEqual({
+      msg: `Dinosaur Island has been removed from your collection.`
+    });
+  });
+});
