65d9c16a67ab2121989dd8dbae0e2c9236ac9b23
Jared Enck <jaredenck86@gmail.com>
2023-05-17 00:49:21 -0400

    custom hooks for form input control and using local storage

---
+++ game-night/src/hooks/useFields.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+/**Controls form fields */
+
+export default function useFields(initialState) {  
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value
+    }));
+  };
+
+  return [formData, handleChange, setFormData];
+};
