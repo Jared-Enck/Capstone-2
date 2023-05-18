117d1e34f0aa70822c51b23de08be50920ce761d
Jared Enck <jaredenck86@gmail.com>
2023-05-18 17:13:26 -0400

    updated LoginForm

--- game-night/src/components/LoginForm.js
+++ game-night/src/components/LoginForm.js
@@ -1,16 +1,28 @@
 import React, { useState, useContext } from "react";
 import UserContext from '../context/UserContext';
-import { 
-  Box,
+import {
   InputLabel,
   FormControl,
   InputAdornment,
   IconButton,
-  OutlinedInput,
-  Button
+  Stack
 } from "@mui/material";
 import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { 
+  PrimaryButton, 
+  ErrorSpan,
+  FormBox,
+  FormOutlinedInput
+} from "./styled";
 import useFields from "../hooks/useFields";
+
+const genError = (err, idx) => {
+  return (
+    <ErrorSpan key={idx}>
+      {err}
+    </ErrorSpan>
+  )
+}
 
 export default function LoginForm() {
   const [showPassword, setShowPassword] = useState(false);
@@ -18,8 +30,12 @@ export default function LoginForm() {
     username: '', 
     password: ''
   };
-  const [formData,handleChange] = useFields(initialState);
-  const [formErrors, setFormErrors] = useState([]);
+  const [
+    formData, 
+    handleChange,
+    formErrors,
+    setFormErrors
+  ] = useFields(initialState);
   const {loginUser} = useContext(UserContext);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -37,59 +53,69 @@ export default function LoginForm() {
   }
 
   return (
-    <Box 
+    <FormBox 
       component="form"
       onSubmit={handleSubmit}
+      autoComplete="off"
+      margin="auto"
     >
-      <h2>
-        Login
-      </h2>
-      <FormControl>
-        <InputLabel 
-          htmlFor="username"
-        >
-          Username
-        </InputLabel>
-        <OutlinedInput
-          type="text"
-          label="Username"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          autoFocus
-        />
-      </FormControl>
-      <FormControl>
-        <InputLabel 
-          htmlFor="password"
-        >
-          Password
-        </InputLabel>
-        <OutlinedInput
-          type={showPassword ? 'text' : 'password'}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </FormControl>
-      <FormControl>
-        <Button type="submit" size="medium">
-          Submit
-        </Button>
-      </FormControl>  
-    </Box>
+      <Stack spacing={2}>
+        <FormControl>
+          <InputLabel 
+            htmlFor="username"
+          >
+            Username
+          </InputLabel>
+          <FormOutlinedInput
+            type="text"
+            label="Username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            autoFocus
+          />
+        </FormControl>
+
+        <FormControl>
+          <InputLabel 
+            htmlFor="password"
+          >
+            Password
+          </InputLabel>
+          <FormOutlinedInput
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </FormControl>
+        <FormControl>
+        {
+          formErrors
+            ? formErrors.map((e,idx) => genError(e,idx))
+            : null
+        }
+          <PrimaryButton
+            type="submit"
+            size="medium"
+          >
+            Login
+          </PrimaryButton>
+        </FormControl>
+      </Stack>
+    </FormBox>
   );
 };
