3731214a61da458efb7a52784e21eda5f6ed85b0
Jared Enck <jaredenck86@gmail.com>
2023-05-18 18:24:55 -0400

    updated to controlled comp

--- game-night/src/components/SearchForm.js
+++ game-night/src/components/SearchForm.js
@@ -11,18 +11,39 @@ import {
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchForm() {
+  const initialState = {
+    search: ''
+  }
+
+  const [
+    formData, 
+    handleChange
+  ] = useFields(initialState);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log('Searching...',formData)
+  }
+
   return (
-    <FormControl>
-      <Search>
-        <SearchIconWrapper>
-          <SearchIcon />
-        </SearchIconWrapper>
-        <StyledInputBase
-          name="search"
-          placeholder="Search…"
-          inputProps={{ 'aria-label': 'search' }}
-        />
-      </Search>
-    </FormControl>
+    <form 
+      onSubmit={handleSubmit}
+      autoComplete="off"
+    >
+      <FormControl>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            name="search"
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+            onChange={handleChange}
+            value={formData.search}
+          />
+        </Search>
+      </FormControl>
+    </form>
   )
 }
