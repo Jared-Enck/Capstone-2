05ab78048575700cfb1e90f979b77894afad20e3
Jared Enck <jaredenck86@gmail.com>
2023-05-18 17:10:38 -0400

    add new theme colors

--- game-night/src/theme.js
+++ game-night/src/theme.js
@@ -1,21 +1,26 @@
 import '@fontsource/roboto/400.css'
 import { createTheme } from '@mui/material/styles';
-import { blue, red } from '@mui/material/colors';
+import { blue, grey, red, common } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
       main: blue[500],
+      light: blue[300],
+      dark: blue[700],
+      contrastText: common.white
+    },
+    secondary: {
+      main: grey[300],
+      light: grey[100],
+      dark: grey[500],
+      contrastText: common.black
     },
     error: {
       main: red.A400,
-    },
+    }
   },
-  typography: {
-    fontFamily: 'Roboto',
-    textAlign: 'center'
-  }
 });
 
 export default theme;
