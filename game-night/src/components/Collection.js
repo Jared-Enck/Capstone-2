5bab1cb377c5eeffe5c447ec66df331c5dc34aa2
Jared Enck <jaredenck86@gmail.com>
2023-05-18 00:01:25 -0400

    create collection component to list user games

---
+++ game-night/src/components/Collection.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+import { Container, Grid, Typography } from "@mui/material";
+
+export default function Collection() {
+  const {currentUser} = useContext(UserContext);
+
+  const noGamesMsg = (
+    <Typography variant="h5">
+      No games yet? Try searching for games and then adding them to your collection.
+    </Typography>
+  )
+
+  return (
+    <Container maxWidth="xl">
+      <Grid 
+        container
+        justifyContent="center"
+      >
+        {
+          currentUser.games
+           ? currentUser.games.map(g => {
+            <Grid key={g.id} item>
+              <span>
+                {g.name}
+              </span>
+            </Grid>
+           })
+           : noGamesMsg
+        }
+      </Grid>
+    </Container>
+  );
+};
