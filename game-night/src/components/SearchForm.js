4d66cff969d308a5d5f06f09ce2f9f3d48bf7a40
Jared Enck <jaredenck86@gmail.com>
2023-05-18 17:16:11 -0400

    updated SearchForm

--- game-night/src/components/SearchForm.js
+++ game-night/src/components/SearchForm.js
@@ -1,26 +1,28 @@
 import React, { useContext, useEffect } from "react";
 import useFields from "../hooks/useFields";
 import { 
-  OutlinedInput,
-  FormControl,
-  InputLabel
+  FormControl
 } from "@mui/material";
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase
+} from './styled'
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchForm() {
   return (
     <FormControl>
-      <InputLabel>
-        Search
-      </InputLabel>
-      <OutlinedInput
-        type="search"
-        label="Search"
-        name="username"
-        placeholder="Search..."
-        value={formData.searchTerm}
-        onChange={handleChange}
-      />
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          name="search"
+          placeholder="Search…"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
     </FormControl>
   )
 }
