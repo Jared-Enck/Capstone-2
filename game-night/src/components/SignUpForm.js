4ac449f221f5ee423343ad2063e15de52c4fdbb2
Jared Enck <jaredenck86@gmail.com>
2023-05-18 17:13:47 -0400

    updated SignUpForm

--- game-night/src/components/SignUpForm.js
+++ game-night/src/components/SignUpForm.js
@@ -1,9 +1,139 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import UserContext from '../context/UserContext';
+import {
+  InputLabel,
+  FormControl,
+  Stack,
+  InputAdornment,
+  IconButton
+} from "@mui/material";
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  FormBox,
+  PrimaryButton, 
+  ErrorSpan,
+  FormOutlinedInput
+} from "./styled";
+import useFields from "../hooks/useFields";
+
+const genError = (err, idx) => {
+  return (
+    <ErrorSpan key={idx}>
+      {err}
+    </ErrorSpan>
+  )
+}
 
 export default function SignUpForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const initialState = {
+    username: '', 
+    password: '',
+    email: '',
+  };
+  const [
+    formData, 
+    handleChange,
+    formErrors,
+    setFormErrors
+  ] = useFields(initialState);
+  const {registerUser} = useContext(UserContext);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await registerUser(formData);
+    if (!result.success) {
+      setFormErrors(result.err);
+    };
+  };
+
   return (
-    <h2>
-      Sign Up!
-    </h2>
+    <FormBox 
+      component="form"
+      onSubmit={handleSubmit}
+      autoComplete="off"
+      margin="auto"
+    >
+      <Stack spacing={2}>
+        <FormControl>
+          <InputLabel 
+            htmlFor="username"
+          >
+            Username
+          </InputLabel>
+          <FormOutlinedInput
+            type="text"
+            label="Username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            autoFocus
+          />
+        </FormControl>
+
+        <FormControl>
+          <InputLabel 
+            htmlFor="password"
+          >
+            Password
+          </InputLabel>
+          <FormOutlinedInput
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl>
+          <InputLabel 
+            htmlFor="email"
+          >
+            Email
+          </InputLabel>
+          <FormOutlinedInput
+            type="text"
+            label="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl>
+          {
+            formErrors
+              ? formErrors.map((e,idx) => genError(e,idx))
+              : null
+          }
+
+          <PrimaryButton
+            type="submit"
+            size="medium"
+          >
+            Sign Up
+          </PrimaryButton>
+        </FormControl>
+      </Stack>
+    </FormBox>
   );
 };
