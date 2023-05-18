1424876dca702d34657bd03ac6bda636ff6c431b
Jared Enck <jaredenck86@gmail.com>
2023-05-18 18:30:28 -0400

    updated styles to align center

--- game-night/src/components/CircularLoading.js
+++ game-night/src/components/CircularLoading.js
@@ -3,7 +3,7 @@ import { CircularProgress, Box } from '@mui/material';
 
 export default function CircularLoading() {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
       <CircularProgress />
     </Box>
   );
