c5dba1ef479b9d0fed9ea008267d32b2ba5c16ab
Jared Enck <jaredenck86@gmail.com>
2023-05-18 17:11:51 -0400

    updated useFields to also track formErrors

--- game-night/src/hooks/useFields.js
+++ game-night/src/hooks/useFields.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
-
+import {setFormErrors} from "../components/LoginForm"
 /**Controls form fields */
 
 export default function useFields(initialState) {  
   const [formData, setFormData] = useState(initialState);
+  const [formErrors, setFormErrors] = useState([]);
 
   const handleChange = (e) => {
     const {name,value} = e.target;
@@ -11,7 +12,16 @@ export default function useFields(initialState) {
       ...fData,
       [name]: value
     }));
+    
+    if (formErrors) {
+      setFormErrors([]);
+    }
   };
 
-  return [formData, handleChange, setFormData];
+  return [
+    formData, 
+    handleChange,
+    formErrors,
+    setFormErrors
+  ];
 };
