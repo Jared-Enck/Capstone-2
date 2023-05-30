9817ae06753f15167e1805c9d4786a3ec5e55ae8
jared <jaredenck86@gmail.com>
2023-05-30 17:58:12 -0400

    create prelimanary comp for displaying search results

---
+++ game-night/src/components/SearchBoxResults.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from "react";
+import { 
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon
+} from "@mui/material";
+
+export default function SearchBoxResults({listGames}) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Menu
+      anchorEl={anchorEl}
+      id="search-results"
+      open={open}
+      onClose={handleClose}
+      onClick={handleClose}
+      PaperProps={{
+        elevation: 0,
+        sx: {
+          overflow: 'visible',
+          filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+          mt: 1.5,
+          '& .MuiAvatar-root': {
+            width: 32,
+            height: 32,
+            ml: -0.5,
+            mr: 1,
+            bgcolor:'primary.light'
+          },
+          '&:before': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            top: 0,
+            right: 14,
+            width: 10,
+            height: 10,
+            bgcolor: 'primary.light',
+            transform: 'translateY(-50%) rotate(45deg)',
+            zIndex: 0,
+          },
+        },
+      }}
+      transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+      anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+    >
+      <MenuItem>
+        Mechanics
+      </MenuItem>
+      <Divider />
+    </Menu>
+  );
+};
