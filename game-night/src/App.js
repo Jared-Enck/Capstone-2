2a0937b4116e6d252005135e14445b3e9e5d1176
jared <jaredenck86@gmail.com>
2023-05-30 17:53:59 -0400

    import DataProvider and wrap necessary comps

--- game-night/src/App.js
+++ game-night/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import UserProvider from "./context/UserProvider";
+import DataProvider from "./context/DataProvider";
 import Navbar from "./components/Navbar";
 import AllRoutes from "./AllRoutes";
 // import Footer from "./components/Footer";
@@ -11,10 +12,12 @@ export default function App() {
   return (
     <UserProvider>
       <ThemeProvider theme={theme}>
-        <Navbar />
-        <main>
-          <AllRoutes />
-        </main>
+        <DataProvider>
+          <Navbar />
+          <main>
+            <AllRoutes />
+          </main>
+        </DataProvider>
       </ThemeProvider>
       {/* <Footer /> */}
     </UserProvider>
