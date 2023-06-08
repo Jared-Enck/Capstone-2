f9dab3fa253d1f15d315e74c1116c960d4f600c4
jared <jaredenck86@gmail.com>
2023-06-08 19:37:56 -0400

    add media container and game decsription

--- game-night/src/components/games/GameDetails.js
+++ game-night/src/components/games/GameDetails.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useContext, useEffect, Suspense, lazy } from "react";
+import DataContext from "../../context/DataContext";
+import CircularLoading from "../common/CircularLoading";
+import {
+  Container,
+  Box,
+  Typography
+} from "@mui/material"
 
-export default function GameDetails({game}) {
+const MediaContainerComp = lazy(
+  () => import("./MediaContainer")
+);
+
+export default function GameDetails() {
+  const { game } = useContext(DataContext);
+
+  useEffect(() => {
+    console.log(game)
+  },[])
+  
+  const images = game.detail_images || [];
+  const videos = game.videos || [];
+
   return (
-    <h1>
-      {game.name}
-    </h1>
+    <Container maxWidth={"xl"}>
+      <Typography variant={"h3"}>
+        {game.name}
+      </Typography>
+      <Box sx={{
+        width: 400
+      }}>
+        <Typography variant={"h6"}>
+          Description
+        </Typography>
+        <p>
+          {game.description_preview}
+        </p>
+      </Box>
+      {
+        images.length
+        ? (
+          <Suspense fallback={<CircularLoading />}>
+            <MediaContainerComp header="Images" items={images} />
+          </Suspense>
+        )
+        : null
+      }
+      {
+        videos.length
+        ? (
+          <Suspense fallback={<CircularLoading />}>
+            <MediaContainerComp header="Videos" items={videos} isVideos={true} />
+          </Suspense>
+        )
+        : null
+      }
+    </Container>
   );
 };
