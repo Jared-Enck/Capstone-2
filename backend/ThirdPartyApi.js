9dfb8be86deccbd9b749090fd816a516b0458a1f
jared <jaredenck86@gmail.com>
2023-06-08 19:33:52 -0400

    add routes for caching games

--- backend/ThirdPartyApi.js
+++ backend/ThirdPartyApi.js
@@ -1,17 +1,22 @@
 const axios = require("axios");
 const { CLIENT_ID, API_BASE_URL } = require("./config");
+const moment = require('moment');
 const fields = require('./helpers/gameObjFields');
 const NodeCache = require("node-cache");
 const myCache = new NodeCache();
+const gamesCache = new NodeCache();
+const moreThanADay = require('./helpers/moreThanADay');
+
+let APICachedDate;
 
 class ThirdPartyApi {
   static async request(endpoint, data = {}, method = "get") {
     console.debug("API Call:", endpoint, data, method);
 
     const url = `${API_BASE_URL}/${endpoint}`;
-    
+
     const params = (method === "get")
-      ? {...data, client_id: CLIENT_ID}
+      ? { ...data, client_id: CLIENT_ID }
       : {};
 
     try {
@@ -22,61 +27,73 @@ class ThirdPartyApi {
       throw Array.isArray(message) ? message : [message];
     };
   };
-  
+
   // Individual API routes
 
   // Get common data
   static async cacheCommon() {
-    if (!myCache.has('games') || !myCache.has('mechanics') || !myCache.has('categories')) {
+    if (!myCache.has('mechanics') || !myCache.has('categories')) {
       const results = await Promise.allSettled([
-        this.request('/search',{ fields, limit: 100, order_by: 'rank' }),
         this.request('game/mechanics'),
         this.request('game/categories'),
       ]);
-  
-      const failed = results.filter(r => r.status === 'rejected');
-  
-      if (failed.length) console.error({ errors: failed });
-      
-      const success = myCache.mset([
-        { key: 'games', val: results[0].value.games, ttl: 86400 },
-        { key: 'mechanics', val: results[1].value.mechanics, ttl: 86400 },
-        { key: 'categories', val: results[2].value.categories, ttl: 86400 }
-      ]);
-      const msg = success ? 'Successfully cached data.' : 'Failed to cache data.';
 
-      return { msg };
+      const failed = results.filter(r => r.status === 'rejected');
+
+      if (failed.length) console.error({ errors: failed });
+
+      const success = myCache.mset([
+        { key: 'mechanics', val: results[0].value.mechanics, ttl: 86400 },
+        { key: 'categories', val: results[1].value.categories, ttl: 86400 }
+      ]);
+      const msg = success
+        ? { success: 'Successfully cached data.' }
+        : { failure: 'Failed to cache data.' };
+
+      return msg;
     };
   };
 
-  static checkCache(term) {
-    const { 
-      games, 
-      mechanics, 
-      categories 
-    } = myCache.mget(['games', 'mechanics', 'categories']);
-    const results = {};
-    const regexp = new RegExp(term, 'i');
+  static async getCommonCache() {
+    const isDayOld = moreThanADay(APICachedDate);
+    if (!APICachedDate || isDayOld) {
+      const msg = await this.cacheCommon();
+      if (msg.success) {
+        APICachedDate = moment().format()
+      };
+    };
+    return APICachedDate;
+  };
 
-    results.foundMechanics = mechanics.filter(m => regexp.test(m.name));
-    results.foundCategories = categories.filter(c => regexp.test(c.name));
-    results.foundGames = games.filter(g => regexp.test(g.name))
+  static checkCommon(term) {
+    if (term) {
+      const {
+        mechanics,
+        categories
+      } = myCache.mget(['mechanics', 'categories']);
+      const results = {};
+      const regexp = new RegExp(term, 'i');
+  
+      results.foundMechanics = mechanics.filter(m => regexp.test(m.name));
+      results.foundCategories = categories.filter(c => regexp.test(c.name));
 
-    return results;
+      return results;
+    };
+  };
+
+  static checkGames(gameID) {
+    const found = gamesCache.get(gameID);
+    return found;
   };
 
   static async getSearchResults(term) {
-    const results = this.checkCache(term);
-    const cachedGames = results.foundGames;
-
-    if (!cachedGames.length || cachedGames.length < 5) {
-      const data = { 
-        name: term, 
-        fields 
-      }
-      const { games } = await this.request('/search', data);
-      results.foundGames = games;
+    const results = this.checkCommon(term);
+    const data = {
+      name: term,
+      fields
     }
+    const { games } = await this.request('/search', data);
+    results.foundGames = games.splice(0,10);
     return results;
   };
 
@@ -85,16 +102,40 @@ class ThirdPartyApi {
       ...query,
       fields
     }
-    const { games } = await this.request('/search',data);
+    const { games } = await this.request('/search', data);
     return games;
   };
 
   static cacheGame(game) {
-    if (!myCache.has('games')) {
-      myCache.set('games', [game], [ 14400 ]);
-    };
-    const games = myCache.get('games');
-    games.push(game)
+    const found = gamesCache.get(game.id);
+    if (!found) gamesCache.set(game.id, game, [14400]);
+  };
+
+  static async getGameMedia({gameID, mechanicIDs, categoryIDs}) {
+    const data = {game_id: gameID}
+    const setMechanics = new Set(mechanicIDs);
+    const setCategories = new Set(categoryIDs);
+    const { mechanics, categories } = myCache.mget(['mechanics', 'categories']);
+
+    const mechanicsRes = mechanics.filter(m => {
+      if (setMechanics.has(m.id)) return m.name
+    });
+
+    const categoriesRes = categories.filter(c => {
+      if (setCategories.has(c.id)) return c.name
+    })
+
+    const results = await Promise.allSettled([
+      this.request('/game/images', data),
+      this.request('/game/videos', data)
+    ]);
+
+    return {
+      mechanicsRes,
+      categoriesRes,
+      detail_images: results[0].value.images,
+      videos: results[1].value.videos
+    }
   };
 };
 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,39 +1,82 @@
 "use strict";
 
-/** Routes for cache data. */
-
-const jsonschema = require("jsonschema");
+/** Routes for third party api. */
 
 const express = require("express");
 const router = new express.Router();
-const { BadRequestError } = require("../expressError");
 const ThirdPartyApi = require("../ThirdPartyApi");
 
-/** GET /api/cache: => { 
- *  games: [{game},...],
- *  mechanics: [{mechanic},...],
- *  categories: [{category},...]
- * }
- *
- * Returns json for top rated games limit 100, mechanics, and categories.
+/** GET /api/cache => { date }
+ * 
+ * Checks ApiCacheDate
+ * and requests data if no date or date past 24 hrs.
+ * 
+ * Returns json date when cache last updated.
  *
  * Authorization required: none
  */
 
 router.get("/cache", async function (req, res, next) {
   try {
-    const msg = await ThirdPartyApi.cacheCommon();
-    return res.json(msg);
+    const date = await ThirdPartyApi.getCommonCache();
+    return res.json({ date });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /api/cache/games => { game }
+ * 
+ * Gets game if in cache
+ * 
+ * Returns json game obj or undefined.
+ *
+ * Authorization required: none
+ */
+
+router.get("/cache/games", function (req, res, next) {
+  try {
+    const { gameID } = req.query;
+    const result = ThirdPartyApi.checkGames(gameID);
+    return res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /api/cache/games => { date }
+ * 
+ * Checks cache if game already in cache
+ * if not, cache game
+ * 
+ * Returns json date when cache last updated.
+ *
+ * Authorization required: none
+ */
+
+router.post("/cache/games", function (req, res, next) {
+  try {
+    const game = req.body;
+    ThirdPartyApi.cacheGame(game);
+    return res.status(201);
   } catch (err) {
     return next(err);
   }
 });
 
 /** GET /api/search: => { 
- *  searchTerm: 'some string'
+ *  foundMechanics: [obj,...],
+    foundCategories: [obj,...],
+    foundGames: [obj,...],
  * }
+ * 
+ * Takes search term and checks cache for matches,
+ * sends request to api for games that match
  *
- * Returns json for 
+ * Returns json for found items
+ * 
+ * else sends request to api for games that match 
+ *  category or mechanic from query string
  *
  * Authorization required: none
  */
@@ -42,13 +85,42 @@ router.get("/search", async function (req, res, next) {
   try {
     const query = req.query
     const term = query.term;
+    let results;
     if (term) {
-      const results = await ThirdPartyApi.getSearchResults(term.toLowerCase());
-      return res.json(results);
+      results = await ThirdPartyApi.getSearchResults(term.toLowerCase());
+    } else {
+      results = await ThirdPartyApi.getRefinedResults(query);
     }
-    const results = await ThirdPartyApi.getRefinedResults(query);
     return res.json(results);
   } catch (err) {
+    return next(err);
+  }
+});
+
+/** Get /api/game_media 
+ * 
+ * {gameID: '78ZDzlpvdb', mechanicIDs: [id,...], categoryIDs: [id,...]}
+ *  => {
+ *    mechanicsRes: [obj,...];
+      categoriesRes: [obj,...];
+      detail_images: [obj,...];
+      videos: [obj,...];
+ * }
+ * 
+ * Gets names for mechanics and categories from id arrays
+ * 
+ * Requests api for game images and videos for gameID
+ * 
+ * Returns json obj with all results
+ * 
+ * Authorization required: none
+*/
+
+router.get("/game_media", async function (req, res, next) {
+  try {
+    const results = await ThirdPartyApi.getGameMedia(req.query);
+    return res.json(results);
+  } catch {
     return next(err);
   }
 });
