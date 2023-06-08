a8c17788bc7ecf8133859841d6d3c3d958814641
jared <jaredenck86@gmail.com>
2023-06-08 19:35:46 -0400

    create comp for games list in search box results

---
+++ game-night/src/components/search/GamesList.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  List,
+  ListItemAvatar,
+  ListItemText,
+  ListItemButton,
+  Avatar
+} from "@mui/material";
+
+export default function GamesList({ items, handleGameClick }) {
+  return (
+    <List
+      sx={{ width: "100%" }}
+    >
+      {
+        items.length
+        ? items.map((i,idx) => (
+          <ListItemButton 
+            alignitems={"flex-start"} 
+            key={i.id} 
+            sx={{
+              padding:".3rem .3rem",
+              fontSize: ".7rem"
+            }}
+            onClick={() => handleGameClick(idx, i.id)}
+          >
+            <ListItemAvatar>
+              <Avatar 
+                sx={{
+                  height: "2.2rem",
+                  width: "2.2rem",
+                  borderRadius: "3px"
+                }}
+                alt={i.name} 
+                src={i.images["thumb"]} 
+              />
+            </ListItemAvatar>
+              <ListItemText sx={{
+                alignSelf: "center",
+                paddingRight: ".5rem",
+                margin: "0",
+                marginLeft: "-.3rem"
+              }}>
+                {i.name}
+              </ListItemText>
+          </ListItemButton>
+        ))
+        : null
+      }
+    </List>
+  );
+};
