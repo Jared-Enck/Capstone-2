f82da4f020c641e0c21327a456f49766e912e81d
jared <jaredenck86@gmail.com>
2023-06-09 20:44:46 -0400

    create media card comp for videos and imgs

---
+++ game-night/src/components/games/MediaCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  CardMedia,
+  Card,
+  CardActionArea,
+} from "@mui/material";
+import VideoOverlay from "./VideoOverlay";
+
+export default function MediaCard({
+  item, 
+  size, 
+  width, 
+  height, 
+  handleClick,
+  isVideo
+}) {
+  return (
+    <Card
+      sx={{
+        height: height,
+        width: width,
+        boxShadow: 0
+      }}
+    >
+      <CardActionArea
+        onClick={() => handleClick(item.url)}
+      >
+        {
+          isVideo
+            ? <VideoOverlay title={ item.title } />
+            : null
+        }
+        <CardMedia
+          sx={{
+            height: height,
+            width: width,
+            objectFit: "fill"
+          }}
+          component="img"
+          image={item[size] || item.image_url}
+          alt={item.name || item.title}
+        />
+      </CardActionArea>
+    </Card>
+  )
+};
