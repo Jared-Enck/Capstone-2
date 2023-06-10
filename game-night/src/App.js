194a33e8d72b476f3c5b16a509cbfb30fd201086
jared <jaredenck86@gmail.com>
2023-06-09 20:54:26 -0400

    add boolean state for light theme/ dark theme

--- game-night/src/App.js
+++ game-night/src/App.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
+import { themeLight, themeDark } from "./theme";
 import UserProvider from "./context/UserProvider";
 import DataProvider from "./context/DataProvider";
 import Navbar from "./components/navbar/Navbar";
 import AllRoutes from "./AllRoutes";
+import { CssBaseline } from "@mui/material";
 // import Footer from "./components/Footer";
 
 export default function App() {
+  const [light, setLight] = useState(true);
   return (
-    <UserProvider>
-      <ThemeProvider theme={theme}>
+    <UserProvider setLight={setLight}>
+      <ThemeProvider theme={light ? themeLight : themeDark}>
+        <CssBaseline />
         <DataProvider>
           <Navbar />
-          <main>
-            <AllRoutes />
-          </main>
+          <AllRoutes />
         </DataProvider>
-      </ThemeProvider>
       {/* <Footer /> */}
+      </ThemeProvider>
     </UserProvider>
   );
 };
