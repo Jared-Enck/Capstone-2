f285a9ddfd59e589612518dd02601a689aa5fb51
jared <jaredenck86@gmail.com>
2023-06-12 19:47:44 -0400

    updated for theme change

--- game-night/src/components/account/UserAccountMenu.js
+++ game-night/src/components/account/UserAccountMenu.js
@@ -53,22 +53,22 @@ export default function UserAccountMenu() {
             overflow: 'visible',
             filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
             mt: 1.5,
+            backgroundColor: 'primary.light',
             '& .MuiAvatar-root': {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
-              bgcolor:'primary.light'
+              bgcolor:'primary.main'
             },
             '&:before': {
-              content: '""',
               display: 'block',
               position: 'absolute',
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'primary.light',
+              bgcolor: 'primary.contrastText',
               transform: 'translateY(-50%) rotate(45deg)',
               zIndex: 0,
             },
