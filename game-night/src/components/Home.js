ad9e4fe8818195cb9ce2fd5adc7aefd9b0e1949c
jared <jaredenck86@gmail.com>
2023-06-16 16:30:01 -0400

    created theme toggle switch to test theme toggling

--- game-night/src/components/Home.js
+++ game-night/src/components/Home.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Switch
+} from "@mui/material"
+import styled from "@emotion/styled";
+import UserContext from "../context/UserContext";
+
+const StyledSwitch = styled(Switch)(({ theme }) => ({
+  color: theme.palette.primary.contrastText
+}));
 
 export default function Home() {
+  const { currentUser, isDark, handleThemeToggle } = useContext(UserContext);
+  const [checked, setChecked] = useState(isDark);
+
+  useEffect(() => {
+    console.log(currentUser)
+  },[])
+  
+  const handleChange = () => {
+    handleThemeToggle();
+    setChecked(!checked);
+  };
+
   return (
-    <h1>
-      Homepage
-    </h1>
+    <>
+      <h1>
+        Homepage
+      </h1>
+      <StyledSwitch
+        checked={checked}
+        onChange={handleChange}
+      />
+    </>
   );
 };
