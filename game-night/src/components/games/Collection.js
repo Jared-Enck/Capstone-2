922d1d4507d6daf055f2eb5c41ada9926dc56f01
Jared Enck <jaredenck86@gmail.com>
2023-06-16 20:12:19 -0400

    fixed game ids not rendering from games.map

--- game-night/src/components/games/Collection.js
+++ game-night/src/components/games/Collection.js
@@ -1,46 +1,53 @@
 import React, { useCallback, useContext, useEffect } from "react";
 import UserContext from "../../context/UserContext";
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Stack
+} from "@mui/material";
+import ContentContainer from "../common/ContentContainer";
 import GameNightApi from "../../gameNightApi";
 
 export default function Collection() {
-  const {currentUser, userGames, setUserGames} = useContext(UserContext);
-  const games = Array.from(userGames) || [];
-
+  const {currentUser, userGames} = useContext(UserContext);
+  const games = Array.from(userGames);
   // const getGames = useCallback(async () => {
   //   const res = await GameNightApi.getGames(currentUser.username);
   //   console.log(res)
   //   setUserGames(res);
-  // });
+  // },[getGames]);
 
   // useEffect(() => {
   //   getGames();
   // },[getGames]);
   
   const noGamesMsg = (
-    <Typography variant="h5">
+    <Typography sx={{color: "primary.text"}} variant="h5">
       No games yet? Try searching for games and then adding them to your collection.
     </Typography>
   )
 
   return (
-    <Container maxWidth="xl">
-      <Grid 
-        container
-        justifyContent="center"
-      >
-        {
-          games.length
-           ? games.map(g => {
-            <Grid key={g.id} item>
-              <span>
-                {g.id}
-              </span>
-            </Grid>
-           })
-           : noGamesMsg
-        }
-      </Grid>
-    </Container>
+    <Stack>
+      <ContentContainer>
+        <Grid 
+          container
+          direction={"row"}
+          spacing={2}
+        >
+          {
+            games.length
+            ? games.map(g => (
+                <Grid key={g} item>
+                  <Typography sx={{color: "primary.text"}}>
+                    {g}
+                  </Typography>
+                </Grid>
+            ))
+            : noGamesMsg
+          }
+        </Grid>
+      </ContentContainer>
+    </Stack>
   );
 };
