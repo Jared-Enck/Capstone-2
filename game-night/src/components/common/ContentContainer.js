42abf2bec1142c7a051c20cff5b34e1975c1daa5
Jared Enck <jaredenck86@gmail.com>
2023-06-16 20:11:38 -0400

    updated to remove outer box comp

--- game-night/src/components/common/ContentContainer.js
+++ game-night/src/components/common/ContentContainer.js
@@ -1,28 +1,24 @@
 import React from "react";
 import {
-  Box,
   Container
 } from "@mui/material";
 import styled from "@emotion/styled";
 
-const ContentBox = styled(Box)(({ theme }) => ({
+const StyledContainer = styled(Container)(({ theme }) => ({
   margin: 0,
   backgroundColor: theme.palette.primary.main,
   borderRadius: theme.shape.borderRadius,
-  padding: "1.5rem"
+  padding: "1.5rem",
+  justifyContent: "center",
+  alignSelf: "center"
 }));
 
 export default function ContentContainer({ children }) {
   return (
-    <ContentBox>
-      <Container
-        maxWidth={"lg"}
-        sx={{
-          justifyContent: "center"
-        }}
-      >
-        {children}
-      </Container>
-    </ContentBox>
+    <StyledContainer
+      maxWidth={"lg"}
+    >
+      {children}
+    </StyledContainer>
   );
 };
