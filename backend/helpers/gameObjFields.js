fff2e765a84f3e5f46ac88f6db2444be257ba0ab
jared <jaredenck86@gmail.com>
2023-06-28 16:48:22 -0400

    rm fields option from query

--- backend/helpers/gameObjFields.js
+++
@@ -1,5 +0,0 @@
-const fieldsSet = ['id', 'name', 'year_published', 'min_players', 'max_players', 'min_playtime', 'max_playtime', 'min_age', 'description', 'description_preview', 'thumb_url', 'image_url', 'images', 'primary_publisher', 'mechanics', 'categories', 'designers', 'developers'];
-
-const fields = fieldsSet.join(',');
-
-module.exports = fields;
