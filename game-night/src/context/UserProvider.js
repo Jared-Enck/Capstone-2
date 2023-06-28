2a05c990ede3f8a7e0e5b5bae1252f9ec53a0d09
jared <jaredenck86@gmail.com>
2023-06-28 17:19:22 -0400

    mv user games state to DataProvider

--- game-night/src/context/UserProvider.js
+++ game-night/src/context/UserProvider.js
@@ -1,8 +1,7 @@
 import React, {
-  useState, 
-  useEffect, 
-  useCallback, 
-  useMemo 
+  useState,
+  useEffect,
+  useCallback
 } from "react";
 import { useNavigate } from "react-router-dom"
 import UserContext from "./UserContext";
@@ -12,70 +11,40 @@ import jwt_decode from "jwt-decode";
 
 export const TOKEN_STORAGE_ID = "game-night-token";
 
-export default function UserProvider({children, isDark, setDarkMode}) {
+export default function UserProvider({ children, isDark, setDarkMode }) {
   const [currentUser, setCurrentUser] = useState('');
   const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
-  const [gameToAdd, setGameToAdd] = useState('');
-  const [userGames, setUserGames] = useState(new Set());
-  const [games, setGames] = useState('');
   const navigate = useNavigate();
 
   const handleThemeToggle = () => {
     setDarkMode(`${!isDark}`);
   };
 
-  const addGame = useCallback(async () => {
-    if (gameToAdd) {
-      try{
-        const username = currentUser.username;
-        await GameNightApi.addGame(gameToAdd, username);
-        const { games } = await GameNightApi.getGames(username);
-        setUserGames(new Set(games));
-      } catch (err) {
-        console.error('unable to add game', err)
-      }
-    }
-  },[gameToAdd]);
-
-  useEffect(() => {
-    addGame();
-    setGameToAdd('');
-  },[gameToAdd, addGame]);
-
-  const getCollection = useCallback(async () => {
-    if (userGames.size) {
-      // Api needs extra commas at beginnin and end of string
-      const gameIdsStr = `,${Array.from(userGames).join(',')},`;
-      const res = await GameNightApi.getCollection({ids: gameIdsStr});
-      setGames(res.games);
-    }
-  },[userGames]);
-
   async function registerUser(registerData) {
     try {
-        let token = await GameNightApi.register(registerData);
-        setToken(token);
-        return { success: true };
-      } catch (err) {
+      let token = await GameNightApi.register(registerData);
+      setToken(token);
+      return { success: true };
+    } catch (err) {
       console.error('register failed', err);
-      return { success: false, err};
+      return { success: false, err };
     };
   };
 
   async function loginUser(loginData) {
     try {
-        let token = await GameNightApi.login(loginData);
-        setToken(token);
-        return { success: true };
-      } catch (err) {
+      let token = await GameNightApi.login(loginData);
+      setToken(token);
+      navigate('/');
+      return { success: true };
+    } catch (err) {
       console.log(err);
-      return { success: false, err};
+      return { success: false, err };
     };
   };
 
   const logout = () => {
     setCurrentUser('');
-    setUserGames('');
     setToken('');
     navigate('/');
   }
@@ -86,20 +55,16 @@ export default function UserProvider({children, isDark, setDarkMode}) {
         const { username } = jwt_decode(token);
         GameNightApi.token = token;
         const { user } = await GameNightApi.getCurrentUser(username);
-        const { games } = await GameNightApi.getGames(username);
         setCurrentUser(user);
-        setUserGames(new Set(games));
-        navigate('/');
       } catch (err) {
-        console.log(err);
-        setCurrentUser('');
+        console.error('Error: ', err);
       };
     };
-  },[token]);
+  }, [token]);
 
   useEffect(() => {
     getCurrentUser();
-  },[token, getCurrentUser]);
+  }, [token, getCurrentUser]);
 
   // const onHomepage = window.location.pathname === '/';
 
@@ -112,16 +77,11 @@ export default function UserProvider({children, isDark, setDarkMode}) {
           loginUser,
           logout,
           handleThemeToggle,
-          isDark,
-          setGameToAdd,
-          userGames,
-          setUserGames,
-          getCollection,
-          games
+          isDark
         }
       }
     >
-      { children }
+      {children}
     </UserContext.Provider>
   );
 };
