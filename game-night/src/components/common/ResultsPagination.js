b042ae598b323fe623a45dc592e7685df32e7dfb
jared <jaredenck86@gmail.com>
2023-06-29 19:06:23 -0400

    create base unconnected pagination comp with styles, with pageCount algorithm

---
+++ game-night/src/components/common/ResultsPagination.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Pagination
+} from "@mui/material";
+import styled from "@emotion/styled";
+import { Translate } from "@mui/icons-material";
+
+const StyledPagination = styled(Pagination)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  padding: "1rem",
+  "& .MuiPaginationItem-root": {
+    color: theme.palette.primary.text,
+  },
+  "& .MuiButtonBase-root": {
+    "&:hover": {
+      backgroundColor: theme.palette.primary.contrastText,
+      color: theme.palette.primary.main,
+      fontWeight: "bold"
+    }
+  }
+}));
+
+export default function ResultsPagination({ page, handleChange, pageCount = 10 }) {
+  return (
+    <StyledPagination
+      count={pageCount}
+      page={page}
+      onChange={handleChange}
+      shape="rounded"
+      size="large"
+      sx={{
+        justifyContent: "center"
+      }}
+    />
+  );
+};
