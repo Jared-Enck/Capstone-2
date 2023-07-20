ea4f94fdc42d57fc874e5482f0f83c2feccbd7c1
jared <jaredenck86@gmail.com>
2023-07-20 16:11:33 -0400

    restructure useEffects for userData and collection

--- game-night/src/components/games/Collection.js
+++ game-night/src/components/games/Collection.js
@@ -1,8 +1,8 @@
 import React, {
   useState,
-  useCallback,
   useEffect,
-  lazy
+  lazy,
+  useCallback
 } from "react";
 import {
   Grid,
@@ -11,88 +11,43 @@ import {
 import GameCard from "./GameCard";
 import ResultsPagination from "../common/ResultsPagination";
 import usePagination from "../../hooks/usePagination";
-
 const CollectionStatsComp = lazy(
   () => import("./CollectionStats")
 );
 
-const gamesChanged = (a, b) => {
-  let result = false;
-  if (a.length !== b.length) return false;
-  for (let i = 0; i < b.length; i++) {
-
-    if (a[i] !== b[i]) {
-      return false;
-    } else {
-      result = true;
-    }
-  }
-  return result;
-}
-
-export default function Collection({ gameIDs, collection, itemsOnPage }) {
-  const [collectionValue, setCollectionValue] = useState(0);
-  const [pages, setPages] = useState('');
-
-  const getCollectionValue = useCallback(() => {
-    let total = 0;
-    collection.map(g => total += g.msrp);
-    setCollectionValue(total);
-  }, [collection]);
-
-  useEffect(() => {
-    getCollectionValue();
-  }, [getCollectionValue, gameIDs.size]);
+export default function Collection({
+  size,
+  value,
+  collection,
+  itemsOnPage
+}) {
+  const [pageContent, setPageContent] = useState([]);
 
   const [
     page,
-    setPage,
     pageCount,
     handleChange,
-  ] = usePagination(gameIDs.size, itemsOnPage);
+    setPage
+  ] = usePagination(size, itemsOnPage);
 
-  const checkForUpdate = useCallback((selected) => {
-    console.log('checking for update')
-
-    const sameGames = gamesChanged(pages[page], selected);
-    if (!sameGames) {
-      setPages(prev => {
-        const next = { ...prev };
-        next[page] = selected;
-        return next;
-      })
-      if(!pages[page].length) setPage(page - 1);
+  const getPageContent = useCallback(() => {
+    let range = [0, itemsOnPage];
+    if (page !== 1) {
+      const skipAmount = (page - 1) * itemsOnPage;
+      range = range.map(n => n + skipAmount);
     }
-  }, [page, pages]);
-
-  const setPageContent = useCallback(() => {
-    const range = [0, itemsOnPage];
-    const skipAmount = (page - 1) * itemsOnPage;
-    const games = [...collection];
-    let selected;
-
-    if (!pages) {
-      selected = games.splice(range[0], range[1]);
-      console.log('selected:', selected)
-      setPages({ [page]: selected });
-    } else {
-      const skipRange = range.map(n => n + skipAmount);
-      selected = games.splice(skipRange[0], skipRange[1]);
-      if (!pages[page]) {
-        console.log('collection:', games)
-        console.log('selected:', selected)
-        setPages({ ...pages, [page]: selected });
-      } else {
-        checkForUpdate(selected);
-      }
-    }
-  }, [setPages, pages, page, itemsOnPage, collection]);
-
+    const content = collection.slice(range[0], range[1]);
+    setPageContent(content);
+  }, [page, itemsOnPage, setPageContent, collection]);
 
   useEffect(() => {
-    setPageContent();
-    console.log('pages: ', pages)
-  }, [setPageContent, page]);
+    getPageContent()
+  }, [page, getPageContent]);
+
+  useEffect(() => {
+    if (!pageContent.length && page !== 1) setPage(page - 1);
+    // eslint-disable-next-line
+  }, [pageContent.length, setPage]);
 
   return (
     <Grid
@@ -104,22 +59,18 @@ export default function Collection({ gameIDs, collection, itemsOnPage }) {
       <Grid item xs={12}>
         <Divider />
         <CollectionStatsComp
-          gameIDs={gameIDs}
-          value={collectionValue}
+          size={size}
+          value={value}
         />
       </Grid>
       {
-        pages
+        pageContent.length
           ? (
-            pages[page]
-              ? (
-                pages[page].map(g => (
-                  <Grid key={g.id} item>
-                    <GameCard game={g} onProfilePage />
-                  </Grid>
-                ))
-              )
-              : null
+            pageContent.map(g => (
+              <Grid key={g.id} item>
+                <GameCard game={g} onProfilePage />
+              </Grid>
+            ))
           )
           : null
       }
--- game-night/src/components/games/CollectionStats.js
+++ game-night/src/components/games/CollectionStats.js
@@ -4,14 +4,14 @@ import {
   Typography
 } from "@mui/material";
 
-export default function CollectionStats({ gameIDs, value }) {
+export default function CollectionStats({ size, value }) {
   return (
     <Stack spacing={1} sx={{ padding: 2 }}>
       <Typography variant="h5" color={"primary.contrastText"}>
         Collection
       </Typography>
       <Typography color={"primary.text"}>
-        Total Games: {gameIDs.size}
+        Total Games: {size}
       </Typography>
       <Typography color={"primary.text"}>
         Estimated Value: {`$${value.toLocaleString('en-Us')}`}
