e66c710d56a7f3046f301e7ef13032bf4b96a838
jared <jaredenck86@gmail.com>
2023-07-24 15:26:40 -0400

    reduced time for debounce to 300ms

--- game-night/src/hooks/useDebounce.js
+++ game-night/src/hooks/useDebounce.js
@@ -13,7 +13,7 @@ export default function useDebounce(callback) {
       ref.current?.();
     };
 
-    return debounce(func, 500);
+    return debounce(func, 300);
   }, []);
 
   return debouncedCallback;
