bf51633ae16f26df64f09976d7930fc011fdeb50
Jared Enck <jaredenck86@gmail.com>
2023-08-07 12:49:37 -0400

    add limit of 10mb to 'express.json' and add urlencoded with limit of 10mb as well

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ const apiRoutes = require('./routes/api');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(authenticateJWT);
 
 app.use('/auth', authRoutes);
