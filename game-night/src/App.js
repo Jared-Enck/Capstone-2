3bda34dc67a2d708666952648b0c1d57fe918b26
Jared Enck <jaredenck86@gmail.com>
2023-08-07 21:37:34 -0400

    add background image App

--- game-night/src/App.js
+++ game-night/src/App.js
@@ -8,7 +8,20 @@ import Navbar from './components/navbar/Navbar';
 import AllRoutes from './AllRoutes';
 import { CssBaseline } from '@mui/material';
 import useLocalStorage from './hooks/useLocalStorage';
+import bgImage from './static/aksel-fristrup-w7eaCH6ShR4-unsplash.jpg';
+import styled from '@emotion/styled';
 // import Footer from "./components/Footer";
+
+const Background = styled('div')({
+  position: 'fixed',
+  backgroundImage: `url(${bgImage})`,
+  width: '100%',
+  height: '100%',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  zIndex: -1,
+});
 
 export default function App() {
   const [darkMode, setDarkMode] = useLocalStorage('dark-mode-preferred');
@@ -22,6 +35,7 @@ export default function App() {
         <CssBaseline />
         <DataProvider>
           <Navbar />
+          <Background />
           <main>
             <AllRoutes />
           </main>
