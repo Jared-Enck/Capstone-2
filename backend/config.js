9416f7c593f17ea79fd9bcb35d31dc1c3229ef74
jared <jaredenck86@gmail.com>
2023-08-15 18:40:59 -0400

    updated config for production

--- backend/config.js
+++ backend/config.js
@@ -3,15 +3,21 @@ const { env } = process;
 
 const config = {
   SECRET_KEY: env.SECRET_KEY,
+  HOST: env.HOST,
   PORT: +env.PORT || 3001,
   DB_URI: getDatabaseUri,
   BCRYPT_WORK_FACTOR: env.NODE_ENV === 'test' ? 1 : 13,
   CLIENT_ID: env.CLIENT_ID,
   API_BASE_URL: env.API_BASE_URL,
+  USER: env.USER,
 };
 
 function getDatabaseUri() {
+  const { SECRET_KEY, PORT, USER, HOST } = config;
   const dbase = env.NODE_ENV === 'test' ? env.DATABASE_TEST : env.DATABASE;
+  if (env.NODE_ENV === 'production') {
+    return `postgres://${USER}:${SECRET_KEY}@${HOST}:${PORT}/${dbase}`;
+  }
   return `socket:/var/run/postgresql?db=${dbase}`;
 }
 
