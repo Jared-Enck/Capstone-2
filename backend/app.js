72826bed6e44d49ff4bf2f48a0d774d8ea748059
jared <jaredenck86@gmail.com>
2023-08-16 17:43:04 -0400

    fix cors?

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const apiRoutes = require('./routes/api');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ allowedHeaders: 'https://mygamenights.com' }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(authenticateJWT);
