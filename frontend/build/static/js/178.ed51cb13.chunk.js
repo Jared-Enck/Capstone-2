be546ce3483987b4ec853e34544e9a6324b469c0
Jared Enck <jaredenck86@gmail.com>
2023-09-22 17:24:11 -0400

    updated build

---
+++ frontend/build/static/js/178.ed51cb13.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[178],{3178:function(r,e,n){n.r(e),n.d(e,{default:function(){return s}});var t=n(390),o=n(9763),a=n(5793),c=n(5477),i=n(2559);function s(r){var e=r.size;(0,t.useContext)(o.Z).colValue;return(0,i.jsxs)(a.Z,{spacing:1,padding:"1.5rem 1.5rem 0rem 2rem",children:[(0,i.jsx)(c.Z,{variant:"h5",color:"primary.text",children:"Collection"}),(0,i.jsxs)(c.Z,{color:"primary.text",children:["Total Games: ",e]}),(0,i.jsx)(c.Z,{color:"primary.text"})]})}}}]);
+//# sourceMappingURL=178.ed51cb13.chunk.js.map
