aa4bd19a0ee604bff21b44f7ca010a77facf6d0d
Jared Enck <jaredenck86@gmail.com>
2023-09-26 18:15:58 -0400

    create dialog for CardMedia iframe comp

--- frontend/src/components/videos/MediaContainer.js
+++ frontend/src/components/videos/MediaContainer.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import { Grid, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, useMediaQuery } from '@mui/material';
 import MediaCard from './MediaCard';
 import ContentContainer from '../common/ContentContainer';
 import MediaSkeleton from './MediaSkeleton';
+import VideoPlayer from './VideoPlayer';
+import { useTheme } from '@mui/material/styles';
 
 export default function MediaContainer({ items }) {
+  const [open, setOpen] = useState(false);
+  const [video, setVideo] = useState('');
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
   const handleClick = (item) => {
-    console.log(item.id.videoId);
+    setOpen(true);
+    setVideo(item);
   };
 
-  // const handleScroll = useCallback((div) => {
-  //   const bottom = div.scrollHeight - div.scrollTop === div.clientHeight;
-  //   if (bottom) {
-  //     getVideos();
-  //     div.removeEventListener('scroll', () => handleScroll(div));
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   const div = videos.current;
-  //   if (div) {
-  //     div.addEventListener('scroll', () => handleScroll(div));
-  //   }
-  // }, [videos, handleScroll]);
+  const handleClose = () => {
+    setOpen(false);
+    setVideo('');
+  };
 
   return (
     <ContentContainer
       header={'Videos'}
-      headerData={items ? items.length : 0}
       divider
     >
+      {video ? (
+        <VideoPlayer
+          open={open}
+          video={video}
+          handleClose={handleClose}
+          fullScreen={fullScreen}
+        />
+      ) : null}
       <Grid
         sx={{
-          alignItems: 'center',
-          height: 220,
-          overflow: 'auto',
-          marginTop: '.3rem',
+          paddingTop: 1,
         }}
         container
         direction={'row'}
         spacing={2}
+        justifyContent={fullScreen ? 'space-around' : 'space-between'}
       >
         {items ? (
           items.map((i) => (
--- frontend/src/components/videos/VideoPlayer.js
+++ frontend/src/components/videos/VideoPlayer.js
@@ -1,20 +1,73 @@
-import React, { useEffect } from 'react';
-import 'mui-player/dist/mui-player.min.css';
-import MuiPlayer from 'mui-player';
-import { useEffect } from 'react';
+import React from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  Card,
+  CardMedia,
+  IconButton,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
-export default function VideoPlayer() {
-  const mp = new MuiPlayer({
-    container: '#mui-player',
-    title: 'Title',
-    src: 'src',
-    autoplay: true,
-    lang: 'en',
-  });
-
-  useEffect(() => {
-    mp();
-  }, []);
-
-  return <div id='mui-player'></div>;
+export default function VideoPlayer({ open, video, handleClose, fullScreen }) {
+  return (
+    <Dialog
+      fullScreen={fullScreen}
+      open={open}
+      onClose={handleClose}
+      sx={{
+        '& .MuiPaper-root': {
+          bgcolor: 'primary.dark',
+        },
+      }}
+      maxWidth={'xl'}
+      fullWidth
+    >
+      {fullScreen ? (
+        <DialogTitle
+          sx={{
+            padding: 0,
+            display: 'flex',
+          }}
+        >
+          <IconButton
+            aria-label='close'
+            onClick={handleClose}
+            sx={{
+              color: 'primary.muted',
+              marginLeft: 'auto',
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+      ) : null}
+      <Card
+        sx={{
+          position: 'relative',
+          width: '100%',
+          paddingBottom: '56.25%',
+          borderRadius: fullScreen ? 0 : null,
+          display: 'flex',
+        }}
+      >
+        <CardMedia
+          component={'iframe'}
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          title={video.snippet.title}
+          allowFullScreen
+          allow='accelerometer; 
+              autoplay; 
+              encrypted-media; 
+              gyroscope; 
+              picture-in-picture'
+          sx={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            border: 0,
+          }}
+        />
+      </Card>
+    </Dialog>
+  );
 }
