bc2481bd380a984bb97c1f3348168239766a215f
Jared Enck <jaredenck86@gmail.com>
2023-09-29 12:59:31 -0400

    change homescreen prop on GamesListItem to hotListItem, as it's more clear

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -94,7 +94,7 @@ export default function Home() {
                   isLastItem={i === hotGames.length - 1}
                   dimensions={isSmallScreen ? {} : homeListDimensions}
                   isSmallScreen={isSmallScreen}
-                  homepage
+                  hotListItem
                 />
               );
             })
