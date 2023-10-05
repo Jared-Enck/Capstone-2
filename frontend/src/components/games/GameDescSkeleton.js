a50275d12762399d0250f1b68e05d1f7cc6b0f9d
Jared Enck <jaredenck86@gmail.com>
2023-10-05 15:07:55 -0400

    updated skel for mobile

--- frontend/src/components/games/GameDescSkeleton.js
+++ frontend/src/components/games/GameDescSkeleton.js
@@ -1,55 +1,111 @@
-import React from 'react';
-import { Stack, Grid, Divider, Typography } from '@mui/material';
+import React, { useContext } from 'react';
+import DataContext from '../../context/DataContext';
+import {
+  Stack,
+  Grid,
+  Divider,
+  Typography,
+  ListItem,
+  ListItemText,
+  Box,
+} from '@mui/material';
+import styled from '@emotion/styled';
 import BaseSkeleton from '../common/BaseSkeleton';
 import ContentContainer from '../common/ContentContainer';
 
+const PDSkelGrid = styled(Grid)(({ theme }) => ({
+  height: '2rem',
+  [theme.breakpoints.down('sm')]: {
+    justifyContent: 'space-around',
+  },
+}));
+
 export default function GameDescSkeleton() {
+  const { isSmallScreen } = useContext(DataContext);
+
+  const genSkelDesc = () => {
+    const skels = [];
+    const numSkels = isSmallScreen ? 5 : 9;
+    const firstBreak = isSmallScreen ? 2 : 3;
+    const secondBreak = isSmallScreen ? null : 6;
+
+    for (let i = 0; i < numSkels; i++) {
+      const skelWidth = Math.floor(Math.random() * (100 - 90) + 90);
+      if (i === firstBreak || i === secondBreak) {
+        skels.push(<Box height={isSmallScreen ? '1rem' : '1.5rem'} />);
+      } else {
+        skels.push(
+          <BaseSkeleton
+            width={`${skelWidth}%`}
+            height={isSmallScreen ? '1rem' : '1.5rem'}
+          />
+        );
+      }
+    }
+    return skels;
+  };
   return (
     <ContentContainer>
-      <Typography variant='h4'>
-        <BaseSkeleton width='40ch' />
-      </Typography>
-      <Grid
-        container
-        direction={'row'}
-        spacing={3}
-        sx={{
-          padding: '.3rem',
-        }}
-      >
-        <Grid item>
-          <BaseSkeleton width={220} />
-        </Grid>
-      </Grid>
-      <Divider />
-      <Grid
-        container
-        direction={'row'}
-        padding={'1.2rem'}
-      >
-        <Grid
-          item
-          xs={4}
-        >
+      <Stack spacing={1}>
+        <Typography>
           <BaseSkeleton
-            width={300}
-            height={300}
+            width='70%'
+            height={isSmallScreen ? '2rem' : '2.624rem'}
           />
-        </Grid>
-        <Grid
-          item
-          xs={7}
+        </Typography>
+        <PDSkelGrid
+          container
+          direction={'row'}
         >
-          <Stack spacing={2.5}>
-            <BaseSkeleton width='65ch' />
-            <BaseSkeleton width='60ch' />
-            <BaseSkeleton width='50ch' />
-            <BaseSkeleton width='65ch' />
-            <BaseSkeleton width='50ch' />
-            <BaseSkeleton width='55ch' />
-          </Stack>
+          {['', '', ''].map((i, idx) => (
+            <Grid
+              item
+              key={idx}
+              display={'flex'}
+              alignItems={'flex-end'}
+            >
+              <ListItem>
+                <BaseSkeleton
+                  width={isSmallScreen ? '4rem' : '4.816rem'}
+                  height={isSmallScreen ? '1.5rem' : '1.7rem'}
+                />
+              </ListItem>
+            </Grid>
+          ))}
+        </PDSkelGrid>
+        <Divider sx={{ bgcolor: 'primary.dark' }} />
+        <Grid
+          container
+          direction={isSmallScreen ? 'column' : 'row'}
+          justifyContent={isSmallScreen ? 'center' : 'space-around'}
+        >
+          <Grid
+            item
+            display={'flex'}
+            justifyContent={'center'}
+            width={isSmallScreen ? '100%' : 'none'}
+          >
+            <BaseSkeleton
+              width='18rem'
+              height='18rem'
+            />
+          </Grid>
+          <Grid
+            item
+            xs={7}
+          >
+            <Stack
+              spacing={1}
+              padding={isSmallScreen ? 1 : 0}
+            >
+              {genSkelDesc().map((i, idx) => (
+                <div key={idx}>{i}</div>
+              ))}
+              <Box height='2.188rem' />
+            </Stack>
+          </Grid>
         </Grid>
-      </Grid>
+      </Stack>
     </ContentContainer>
   );
 }
